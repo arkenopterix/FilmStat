f50c3e5dab9c5baf809157da113a9a160c88e17e
arkenopterix <briegbk@yahoo.com>
2016-12-25 21:23:17 +0100

    Filmstat working for one film and manual launch

--- FilmStat/filmStat.py
+++ FilmStat/filmStat.py
@@ -4,8 +4,10 @@
 #print(html)
 import urllib
 import urllib.request
+import sys
 import datetime
 from lxml import html
+
 
 url = 'http://www.allocine.fr/film/fichefilm_gen_cfilm=228094.html'
 page = html.fromstring(urllib.request.urlopen(url).read())
@@ -17,6 +19,16 @@ for link in page.xpath("//span[@class='stareval-note']"):
     specTemp.append(link.text.lstrip('\n '))
     #print("Name", link.text, "URL", link.get("href"))
 spec.append(datetime.datetime.now().isoformat())
-spec.append(float(specTemp[0].replace(',','.')))
-spec.append(float(specTemp[1].replace(',','.')))
+spec.append(specTemp[0].replace(',','.'))
+spec.append(specTemp[1].replace(',','.'))
+
+try:
+    with open("/home/brieg/Projects/FilmStat/testfilm.csv","a") as fi:
+        fi.write("\r"+spec[0]+","+spec[1]+","+spec[2])
+        fi.close()
+except:
+    print("Error while loading the file : "+sys.exc_info()[0])
+    exit()
 print(spec)
+
+
